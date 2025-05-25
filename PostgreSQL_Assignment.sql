349c2229785c67c2cbef8b4b73fde1ebc1a5f312
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-05-25 12:22:26 +0600

    create species table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,6 +1,20 @@
--- rangers table --
+
+-- Rangers Table --
 CREATE TABLE rangers(
     ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     region VARCHAR(100) NOT NULL
-)
+);
+
+
+-- Species Table --
+CREATE TABLE species(
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(150) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) CHECK (conservation_status IN ('Endangered', 'Vulnerable', 'Historic'))
+);
+
+
+
